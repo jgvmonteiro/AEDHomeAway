1c555d0f567c291e16edee6f962b6183d876c5c9
docter <docter@DESKTOP-8M0KIDD.lan>
2017-10-07 20:29:41 +0100

    comments

--- src/HomeAway/Visits.java
+++ src/HomeAway/Visits.java
@@ -3,7 +3,9 @@ package HomeAway;
 import java.io.Serializable;
 
 /**
- * 
+ * In the current phase, as you can only have one User and one Home, instead of a home array to store the 
+ * user's visits, you can simply store the home that was visited and the amount of visitations.
+ * This class stores that information.
  * @author chaos_
  */
 public class Visits implements Serializable{
@@ -15,16 +17,25 @@ public class Visits implements Serializable{
 		this.home = home;
 		this.timesVisited = 0;
 	}
-
+	
+	/**
+	 * @return the home which was visited
+	 */
 	public Home getHome(){
-		return this.home;
+		return home;
 	}
 	
+	/**
+	 * @return the amount of visits to home
+	 */
 	public int getVisitations(){
-		return this.timesVisited;
+		return timesVisited;
 	}
 	
+	/**
+	 * Increments the visit counter
+	 */
 	protected void newVisit(){
-		this.timesVisited++;
+		timesVisited++;
 	}
 }
