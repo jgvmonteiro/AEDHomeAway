ef4f875c6684f37fd2e059bfbcaf00d8c53421de
jgvmonteiro <jgvmonteiro@hotmail.com>
2017-10-08 10:57:54 +0100

    HomeAway class simplified, exception now thorwn in UserClass, new comments added, comments edited, methods names edited, etc

--- src/HomeAway/Exceptions/UserIsOwnerException.java
+++ src/HomeAway/Exceptions/UserIsOwnerException.java
@@ -1,6 +1,6 @@
 package HomeAway.Exceptions;
 /**
- * Thrown when a user tries to rent and rate their own property.
+ * Thrown when a user tries to rent and give feedback to his own property.
  * 
  * @author Diogo Tavares (50309) dc.tavares@campus.fct.unl.pt
  * @author Joao Monteiro (51105) jg.monteiro@campus.fct.unl.pt
