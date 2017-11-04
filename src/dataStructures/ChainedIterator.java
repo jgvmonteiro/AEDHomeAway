93f23d0ddfe4c6eb954ff1ff61ba94ed3b32cfd7
docter <docter@DESKTOP-8M0KIDD.lan>
2017-11-04 12:32:59 +0000

    updated with possible solution

--- src/dataStructures/ChainedIterator.java
+++ src/dataStructures/ChainedIterator.java
@@ -22,8 +22,8 @@ public class ChainedIterator<K, V> implements Iterator<Entry<K, V>>{
 	
 	@Override
 	public boolean hasNext() {
-		//return this.current != this.table.length;
-		return this.currentIt.hasNext();
+		return this.current != this.table.length;
+		//return this.currentIt.hasNext();
 	}
 
 	@Override
@@ -31,18 +31,38 @@ public class ChainedIterator<K, V> implements Iterator<Entry<K, V>>{
 		if (!this.hasNext())
             throw new NoSuchElementException();
 		
+		if(this.currentIt.hasNext()) {
+			return currentIt.next();
+		}
 		
+		else findNext();
 		
-		return null;
+		if(this.currentIt == null) {
+			return null;
+		}
+		else return this.currentIt.next();
 	}
 
 	@Override
 	public void rewind() {
 		this.current = -1;
+		findNext();
+	}
+
+	/**
+	 * Moves current into the next position and updates currentIt
+	 */
+	private void findNext() {
+		/*
 		while(this.currentIt == null){	//will always be true on the first execution as currentIt is set to null right before
 			this.current++;
 			this.currentIt = this.table[this.current].iterator();
+		}*/
+		while(this.table[current] == null && this.current < this.table.length) {
+			this.current++;
 		}
+		this.currentIt = this.table[this.current].iterator();
 	}
-
+	
+	
 }
